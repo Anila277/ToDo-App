48e318b1812c96ec59cd58c6f67e978d1a385d54
Anila Nisar <raniyasr@gmail.com>
2022-10-15 14:09:45 -0400

    wip: created TaskList class

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,10 @@
 import imp
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
-def home(request):
-    return HttpResponse('Hello World')
+class TaskList(ListView): 
+    model = Task
+    context_object_name = 'tasks'
 
 # Create your views here.
